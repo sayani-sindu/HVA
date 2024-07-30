f89df2144d852c649d67d2953f8f028613bf26bf
sayani-sindu <sindusayani1002@gmail.com>
2024-07-30 21:42:59 +0530

    Arithemetic operations

---
+++ ArithmeticOperations_Js/arithmetic-operation.js
@@ -0,0 +1,29 @@
+function addNUmbers(num1, num2){
+    return num1 + num2;
+}
+
+function multiplyNumbers(num1, num2){
+    return num1 * num2;
+}
+
+function subtractNumbers(num1, num2){
+    return num1 - num2;
+}
+
+function divideNumbers(num1, num2){
+    if(num2 == 0){
+        return "Error: Division By Zero";
+    }
+    return num1 / num2;
+}
+
+function performArithemetic(num1, num2, callback){
+    return callback(num1, num2);
+}
+
+console.log(performArithemetic(5, 3, addNUmbers));
+console.log(performArithemetic(5, 3, multiplyNumbers));
+console.log(performArithemetic(5, 3, subtractNumbers));
+console.log(performArithemetic(5, 3, divideNumbers));
+
+
